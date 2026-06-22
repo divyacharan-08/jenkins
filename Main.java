31332955df33eb316a39851b0fddb1fdc31c359e
divyacharan-08 <divyacharancheenepalli@gmail.com>
2026-06-22 11:27:37 +0530

    Refactor Main.java to clean up output
    Removed unused variable and adjusted print statements.

--- Main.java
+++ Main.java
@@ -1,9 +1,7 @@
-
 public class Main {
     public static void main(String[] args){
-        int a = 16
-        System.out.println("Hello world");
-        System.out.println("My age is "+ a);
 
+        System.out.println("Hello World");
 
     }
+}
