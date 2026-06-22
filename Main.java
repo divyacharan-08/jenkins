9804186f1735a0026aa9ed62b8b40c07c2ceb379
divyacharan-08 <divyacharancheenepalli@gmail.com>
2026-06-22 10:47:08 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,8 +1,12 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
-
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
         System.out.println("Hello world");
-        System.out.println("Hi");
+        System.out.println("My age is "+ a);
+        scanner.close();
+
 
     }
 }
